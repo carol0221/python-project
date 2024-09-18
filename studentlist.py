5f4ece57c5da0a8cb3153e437098782b725eef80
carol0221 <CarolEngo21@gmail.com>
2024-09-18 19:27:53 +0800

    slicing studentlist.py

--- studentlist.py
+++ studentlist.py
@@ -1,4 +1,3 @@
 studentnames = ["sehun", "kyungsoo", "suho", "kai", "xiumin", "chanyeol", "baekhyun", "chen", "lay", "tao", "luhan", "jeo", "zico", "minghyu", "scoups", "vernon", "minghao", "joshua", "jun", "jay"]
 
-del studentnames[9]
-print(studentnames)
+print(studentnames[1:4])
