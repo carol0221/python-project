2da9e6ac871a79d3c82683ef750b7cdbb7dc0ead
carol0221 <CarolEngo21@gmail.com>
2024-09-18 18:53:53 +0800

    studentsname.py

---
+++ studentlist.py
@@ -0,0 +1,3 @@
+studentnames = ("sehun", "kyungsoo", "suho", "kai", "xiumin", "chanyeol", "baekhyun", "chen", "lay", "tao", "luhan", "jeo", "zico", "minghyu", "scoups", "vernon", "minghao", "joshua", "jun", "jay")
+
+print(studentnames)
